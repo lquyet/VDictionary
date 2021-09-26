c5fcb36aa09cb007d4795885887302b45e811f92
zamiken <zamiken@github>
2021-09-26 16:00:21 +0700

    add Dictionary.java; Word.java and DictionaryManagemment.java

---
+++ src/Dictionary.java
@@ -0,0 +1,8 @@
+public class Dictionary {
+    private Word[] words;
+    private int size;
+    public void addWord(String anh, String viet) {
+        words = new Word[size+1];
+        words[size] = new Word(anh, viet);
+    }
+}
---
+++ src/Word.java
@@ -0,0 +1,8 @@
+public class Word {
+    private String word_target;
+    private String word_explain;
+    Word(String anh, String viet) {
+        word_target = anh;
+        word_explain = viet;
+    }
+}
